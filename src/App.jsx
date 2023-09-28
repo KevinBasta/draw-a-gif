e2c5ab0d8ed258a8de7cfc36284ea29b8a5bb120
Kevin Basta <kevinbastadeveloper@gmail.com>
2023-09-27 23:33:41 -0400

    react test app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+import "./styles.css"
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return []
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos])
+  //setNewItem("sdfsdf")
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+              ...currentTodos,
+              {id: crypto.randomUUID(), title, completed: false },
+            ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          // can't mutate state so returning the todo object with
+          // completed modified
+          return {...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  console.log(todos);
+
+  return ( 
+  <>
+  <NewTodoForm onSubmit={addTodo} />
+  <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+  </> )
 }
-
-export default App
---
+++ src/TodoList.jsx
@@ -0,0 +1,28 @@
+import { TodoItem } from "./TodoItem"
+
+export function TodoList({todos, toggleTodo, deleteTodo}) {
+    
+    
+    
+    return (
+        <>
+        <h1 className="header">Todo list</h1>
+        <ul className="list">
+            {todos.length === 0 && "No Todos"}
+            {todos.map(todo => {
+            return (
+                <TodoItem 
+                    // or {...todo} for first 3
+                    id={todo.id}
+                    completed={todo.completed}
+                    title={todo.title}
+                    key={todo.id}
+                    toggleTodo={toggleTodo}
+                    deleteTodo={deleteTodo}
+                />
+            )
+            })}
+        </ul>
+        </>
+    )
+}
