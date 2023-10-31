af1933750c7f60cb1bf3f4c226f3fcaf49af6b76
Kevin Basta <kevinbastadeveloper@gmail.com>
2023-10-30 21:27:03 -0400

    download and upload json data for debugging

--- src/menu/GifStorageItem.tsx
+++ src/menu/GifStorageItem.tsx
@@ -8,7 +8,7 @@ interface GifStorageItemsProps {
 }
 
 export function GifStorageItems(props: GifStorageItemsProps) {
-    const [localStorageGIFs, setLocalStorageGIFs] = useState(JSON.parse(localStorage.getItem("GIFS") || "[]"));
+    const [localStorageGIFs, setLocalStorageGIFs] = useState<Array<gifRecord>>(JSON.parse(localStorage.getItem("GIFS") || "[]"));
 
     function deleteSavedGIF(deletingIndex: number) {
         let newGIFs: Array<gifRecord> = [];
@@ -22,13 +22,47 @@ export function GifStorageItems(props: GifStorageItemsProps) {
         setLocalStorageGIFs(() => { return newGIFs });
     }
 
+    function downloadSavedGIF(index: number) {
+        const blob = new Blob([JSON.stringify(localStorageGIFs[index])], { type: "application/json" });
+        const link = document.createElement("a");
+    
+        link.download = localStorageGIFs[index].canvas.canvasName + ".json";
+        link.href = window.URL.createObjectURL(blob);
+        link.dataset.downloadurl = ["application/json", link.download, link.href].join(":");
+    
+        const evt = new MouseEvent("click", {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        });
+    
+        link.dispatchEvent(evt);
+        link.remove()
+    }
+
+    function uploadJsonGIF() {
+        var files = document.getElementById('jsonFile').files;
+        console.log(files);
+        if (files.length <= 0) {
+            return;
+        }
+
+        var fr = new FileReader();
+
+        fr.onload = function(e) { 
+            console.log(e);
+            var result = JSON.parse(e.target.result);
+            var formatted = JSON.stringify(result, null, 2);
+            setLocalStorageGIFs((current) => { return [...current, JSON.parse(fr.result)] });
+            console.log(localStorageGIFs);
+        }
+
+        fr.readAsText(files.item(0), "application/json");
+    }
+
     function getSavedGIFs() {
         if (localStorageGIFs.length == 0) {
-            return (
-                <GifStorageItemWrapper key={crypto.randomUUID()}>
-                    <GifStorageItemTitle>No Saved GIFs</GifStorageItemTitle>
-                </GifStorageItemWrapper>
-            )
+            return;
         }
 
         let savedGIFs: Array<gifRecord> = localStorageGIFs;
@@ -55,6 +89,7 @@ export function GifStorageItems(props: GifStorageItemsProps) {
                     <GifStorageItemTitle title={currentGIF.canvas.canvasName}>{currentGIF.canvas.canvasName || "GIF SAVE #" + (i + 1) }</GifStorageItemTitle>
                     
                     <GifStorageItemButton onClick={e => props.initCanvasFromSave(currentGIF.canvas, currentGIF.frames, currentGIF.globalColorTable)}>load</GifStorageItemButton>
+                    <GifStorageItemButton onClick={e => downloadSavedGIF(i)}>download data</GifStorageItemButton>
                     <GifStorageItemButton onClick={e => deleteSavedGIF(i)}>delete</GifStorageItemButton>
                 </GifStorageItemWrapper>
             )
@@ -73,6 +108,11 @@ export function GifStorageItems(props: GifStorageItemsProps) {
             {
                 getSavedGIFs()
             }
+            <GifStorageItemWrapper key={crypto.randomUUID()}>
+                <GifStorageItemTitle>Upload a json file</GifStorageItemTitle>
+                <input type="file" id="jsonFile" name="filename"></input>
+                <GifStorageItemButton onClick={e => uploadJsonGIF()}>Upload File</GifStorageItemButton>
+            </GifStorageItemWrapper>
         </>
     );
 }
